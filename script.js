430a05177d2d2a2dfadcd7044e35e70011396fad
Briancarlo24 <briancarlo2494@gmail.com>
2023-01-08 07:59:10 -0500

    final

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 const shareIcon = document.querySelector(".fa-share");
 const iconContainer = document.querySelector(".icon-container");
 const picture = document.querySelector(".picture");
-const profile = document.querySelector(".profile");
+const profileContainer = document.querySelector(".profile-container");
 const socialMedia = document.querySelector(".social-media");
 const mobile = window.matchMedia("(max-width: 977px)");
 
-if (mobile.matches) {
-  shareIcon.addEventListener("click", () => {
-    if (mobile.matches) {
-      mobileButton();
-    } else {
-      desktopButton();
-    }
-  });
-} else {
-  shareIcon.addEventListener("click", () => {
-    if (mobile.matches) {
-      mobileButton();
-    } else {
-      desktopButton();
-    }
-  });
-}
-
 function mobileButton() {
-  shareIcon.classList.toggle("icon-color");
   iconContainer.classList.toggle("bg-color");
-  picture.classList.toggle("hidden");
-  profile.classList.toggle("hidden");
+  profileContainer.classList.toggle("hidden");
   socialMedia.classList.toggle("hidden");
+  shareIcon.classList.toggle("icon-color");
 }
 
 function desktopButton() {
-  socialMedia.classList.toggle("overlay");
+  socialMedia.classList.toggle("tooltip");
+  socialMedia.classList.toggle("hidden");
 }
+
+shareIcon.addEventListener("click", function () {
+  if (mobile.matches) {
+    mobileButton();
+  } else {
+    desktopButton();
+  }
+});
+
+document.addEventListener("click", function (event) {
+  const isClickInsideElement = iconContainer.contains(event.target);
+  if (!isClickInsideElement) {
+    socialMedia.classList.remove("tooltip");
+    socialMedia.classList.add("hidden");
+    iconContainer.classList.remove("bg-color");
+    profileContainer.classList.remove("hidden");
+    shareIcon.classList.remove("icon-color");
+  }
+});
