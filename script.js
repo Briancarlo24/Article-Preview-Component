9106b64de9cf0a088b2ed56796a5666d1c6b57b8
Briancarlo24 <Briancarlo2494@gmail.com>
2023-02-09 00:40:50 -0500

    s

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
-const shareIcon = document.querySelector(".fa-share");
-const iconContainer = document.querySelector(".icon-container");
-const picture = document.querySelector(".picture");
-const profileContainer = document.querySelector(".profile-container");
-const socialMedia = document.querySelector(".social-media");
-const mobile = window.matchMedia("(max-width: 977px)");
+const shareButton = document.querySelector('.icon-share-button');
+const person = document.querySelector('.person');
+const social = document.querySelector('.social');
 
-function mobileButton() {
-  iconContainer.classList.toggle("bg-color");
-  profileContainer.classList.toggle("hidden");
-  socialMedia.classList.toggle("hidden");
-  shareIcon.classList.toggle("icon-color");
-}
 
-function desktopButton() {
-  socialMedia.classList.toggle("tooltip");
-  socialMedia.classList.toggle("hidden");
-}
+shareButton.addEventListener("click" , function (e) {
+    e.preventDefault();
+    person.classList.toggle('hidden');
+    social.classList.toggle('hidden-desktop');
+    social.classList.toggle('hidden');
 
-shareIcon.addEventListener("click", function () {
-  if (mobile.matches) {
-    mobileButton();
-  } else {
-    desktopButton();
-  }
 });
 
-document.addEventListener("click", function (event) {
-  const isClickInsideElement = iconContainer.contains(event.target);
-  if (!isClickInsideElement) {
-    socialMedia.classList.remove("tooltip");
-    socialMedia.classList.add("hidden");
-    iconContainer.classList.remove("bg-color");
-    profileContainer.classList.remove("hidden");
-    shareIcon.classList.remove("icon-color");
-  }
-});
+
+
+// ////////////////////////////////OLD Button CODE////////////////////////////////
+
+
+
+// function mobileButton() {
+//   iconContainer.classList.toggle("bg-color");
+//   profileContainer.classList.toggle("hidden");
+//   socialMedia.classList.toggle("hidden");
+//   shareIcon.classList.toggle("icon-color");
+// }
+
+// function desktopButton() {
+//   socialMedia.classList.toggle("tooltip");
+//   socialMedia.classList.toggle("hidden");
+// }
+
+// shareIcon.addEventListener("click", function () {
+//   if (mobile.matches) {
+//     mobileButton();
+//   } else {
+//     desktopButton();
+//   }
+// });
+
+// document.addEventListener("click", function (event) {
+//   const isClickInsideElement = iconContainer.contains(event.target);
+//   if (!isClickInsideElement) {
+//     socialMedia.classList.remove("tooltip");
+//     socialMedia.classList.add("hidden");
+//     iconContainer.classList.remove("bg-color");
+//     profileContainer.classList.remove("hidden");
+//     shareIcon.classList.remove("icon-color");
+//   }
+// });
+
+// ////////////////////////////////OLD Button CODE////////////////////////////////
+
+
